e57a1949b6956ae869e05b70f6459c2715114a65
Kimmo Puputti <kpuputti@gmail.com>
2012-05-03 16:48:00 +0300

    Add build files.

---
+++ dist/perkele-0.1.0.js
@@ -0,0 +1,76 @@
+/*! perkele.js - v0.1.0 - 2012-05-03
+* https://github.com/kpuputti/perkele.js
+* Copyright (c) 2012 Kimmo Puputti; Licensed MIT */
+
+/*global window: false, document: false */
+
+(function () {
+
+    var log = function () {
+        if (window.console && console.log && console.log.apply) {
+            var args = Array.prototype.slice.call(arguments);
+            args.unshift('perkele:');
+            console.log.apply(console, args);
+        }
+    };
+
+    var perkele = {
+        currentIndex: null,
+        slides: []
+    };
+
+    // Show slide with the given index
+    perkele.show = function (index) {
+        if (index < 0 || index >= perkele.slides.length) {
+            return;
+        }
+        var current = document.querySelector('article.current');
+        if (current) {
+            current.className = '';
+        }
+        perkele.slides[index].className = 'current';
+        perkele.currentIndex = index;
+    };
+
+    // Show previous slide
+    perkele.prev = function () {
+        if (perkele.currentIndex !== null) {
+            log('prev');
+            perkele.show(perkele.currentIndex - 1);
+        }
+    };
+
+    // Show next slide
+    perkele.next = function () {
+        if (perkele.currentIndex !== null) {
+            log('next');
+            perkele.show(perkele.currentIndex + 1);
+        }
+    };
+
+    // Handle keyboard event for arrow navigation
+    perkele._onKeyUp = function (event) {
+        var key = event.keyCode;
+        log('key up:', event, 'with key code:', key);
+        if (key === 37) {
+            // arrow left
+            perkele.prev();
+        } else if (key === 39) {
+            // arrow right
+            perkele.next();
+        }
+    };
+
+    // Start slide framework
+    perkele.start = function () {
+        log('start');
+        perkele.slides = document.querySelectorAll('article');
+        if (perkele.slides.length > 0) {
+            perkele.show(0);
+        }
+        document.body.addEventListener('keyup', perkele._onKeyUp, false);
+    };
+
+    window.addEventListener('DOMContentLoaded', perkele.start, false);
+
+}());
